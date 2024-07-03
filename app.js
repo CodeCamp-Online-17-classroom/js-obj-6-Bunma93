7cf0faf4f486b266ae64d24fc54913ab951cb04e
Bunma93 <bunma93@gmail.com>
2024-07-03 11:23:24 +0700

    Js-obj6

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+let user = {
+    // name: "James"
+};
+let isEmpty = (obj) => {
+    for (let key in obj){
+    if (obj.hasOwnProperty(key)) {
+        return false;
+        }
+    } 
+    return true;
+}
+
+console.log(isEmpty(user));
